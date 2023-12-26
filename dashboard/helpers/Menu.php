8e8f7236a608b933db30e28dbe33eab54d58d73a
Máté <mtszita2006@gmail.com>
2023-12-26 16:59:00 +0100

    asd

---
+++ dashboard/helpers/Menu.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Kezdőlap', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'news', 
+			'label' => 'Hírek', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'courses', 
+			'label' => 'Képzések', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'oktatok', 
+			'label' => 'Oktatók', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'infrastructure', 
+			'label' => 'Infrastruktúra', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'gallery', 
+			'label' => 'Galéria', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'faqs', 
+			'label' => 'Kérdések', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'docs', 
+			'label' => 'Dokumentumok', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Felhasználók', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
