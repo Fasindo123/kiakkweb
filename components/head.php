6b624ef7a5e96cc5934a6c9fb6feb3e522d23519
PitorParker <huberpetergyorgy@gmail.com>
2023-12-26 18:34:43 +0100

    asd

--- components/head.php
+++ components/head.php
@@ -1,11 +1,12 @@
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Kaposvári Ágazati Képzőközpont">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="msapplication-TileColor" content="#0E0E0E">
     <meta name="template-color" content="#0E0E0E">
     <meta name="description" content="Index page">
-    <meta name="keywords" content="index, page">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP, MySQL">
     <meta name="author" content="">
     <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/template/logo-black.png">
     <link href="assets/css/style.css?v=2.0.0" rel="stylesheet">
