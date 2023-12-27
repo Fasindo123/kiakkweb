36d4fd48a72cea4421aa1ed91ba6236df1940386
Máté <mtszita2006@gmail.com>
2023-12-27 14:07:55 +0100

    Gyakori kérdések lekérése sql-ből

--- index.php
+++ index.php
@@ -286,46 +286,21 @@
                 </div>
                 <div class="col-lg-6">
                   <div class="accordion" id="accordionFAQ">
-                    <div class="accordion-item scroll-move-up-2">
-                      <h5 class="accordion-header" id="headingOne">
-                      <button class="accordion-button heading-5 color-900 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Where is my order? Quisque molestie</button>
-                      </h5>
-                      <div class="accordion-collapse collapse" id="collapseOne" aria-labelledby="headingOne" data-bs-parent="#accordionFAQ">
-                        <div class="accordion-body font-lg color-600">Vel tenetur officiis ab reiciendis dolor aut quae doloremque est ipsum natus et consequatur animi aut sunt dolores ut quasi rerum. Aut velit velit id quasi velit eum reiciendis laudantium quo galisum incidunt aut velit animi hic temporibus blanditiis sit odit iure. Eum laborum dolores ea molestias fuga qui temporibus accusantium qui soluta aliquid ab vero soluta.</div>
-                      </div>
-                    </div>
-                    <div class="accordion-item scroll-move-up-2">
-                      <h5 class="accordion-header" id="headingTwo">
-                        <button class="accordion-button heading-5 color-900 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Can I cancel or change my order?</button>
-                      </h5>
-                      <div class="accordion-collapse collapse" id="collapseTwo" aria-labelledby="headingTwo" data-bs-parent="#accordionFAQ">
-                        <div class="accordion-body font-lg color-600">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature Id pro doctus mediocrem erroribus, diam nostro sed cu. Ea pri graeco tritani partiendo. Omittantur No tale choro fastidii his, pri cu epicuri perpetua. Enim dictas omittantur et duo, vocent lucilius quaestio mea ex. Ex illum officiis id.</div>
-                      </div>
-                    </div>
-                    <div class="accordion-item scroll-move-up-2">
-                      <h5 class="accordion-header" id="headingThree">
-                        <button class="accordion-button heading-5 color-900 collapsed type=" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Can I cancel or change my order?</button>
-                      </h5>
-                      <div class="accordion-collapse collapse" id="collapseThree" aria-labelledby="headingThree" data-bs-parent="#accordionFAQ">
-                        <div class="accordion-body font-lg color-600">Aut architecto consequatur sit error nemo sed dolorum suscipit 33 impedit dignissimos ut velit blanditiis qui quos magni id dolore dignissimos. Sit ipsa consectetur et sint harum et dicta consequuntur id cupiditate perferendis qui quisquam enim. Vel autem illo id error excepturi est dolorum voluptas qui maxime consequatur et culpa quibusdam in iusto vero sit amet Quis.</div>
-                      </div>
-                    </div>
-                    <div class="accordion-item scroll-move-up-2">
-                      <h5 class="accordion-header" id="headingFour">
-                        <button class="accordion-button heading-5 color-900 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">I have promotional or discount code?</button>
-                      </h5>
-                      <div class="accordion-collapse collapse" id="collapseFour" aria-labelledby="headingFour" data-bs-parent="#accordionFAQ">
-                        <div class="accordion-body font-lg color-600">Eos nostrum aperiam ab enim quas sit voluptate fuga. Ea aperiam voluptas a accusantium similique 33 alias sapiente non vitae repellat et dolorum omnis eos beatae praesentium id sunt corporis. Aut nisi blanditiis aut corrupti quae et accusantium doloribus sed tempore libero a dolorum beatae.</div>
-                      </div>
-                    </div>
-                    <div class="accordion-item scroll-move-up-2">
-                      <h5 class="accordion-header" id="headingFive">
-                        <button class="accordion-button heading-5 color-900 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">What are the delivery types you use?</button>
-                      </h5>
-                      <div class="accordion-collapse collapse" id="collapseFive" aria-labelledby="headingFive" data-bs-parent="#accordionFAQ">
-                        <div class="accordion-body font-lg color-600">Et beatae quae ex minima porro aut nihil quia sed optio dignissimos et voluptates deleniti et nesciunt veritatis et suscipit quod. Est sint voluptate id unde nesciunt non deleniti debitis. Ut dolores tempore vel placeat nemo quo enim reprehenderit eos corrupti maiores et minima quaerat. Quo sequi eaque eum similique sint et autem perspiciatis cum Quis exercitationem quo quos excepturi non ducimus ducimus eos natus velit.</div>
-                      </div>
-                    </div>
+                    <?php
+                      $faqs = sqlQuery("SELECT * FROM faqs");
+                      if ($faqs->num_rows>0) {
+                        while ($faqs_e = $faqs->fetch_assoc()) {
+                          echo '<div class="accordion-item scroll-move-up-2">
+                                  <h5 class="accordion-header" id="headingOne">
+                                  <button class="accordion-button heading-5 color-900 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq_'.$faqs_e["id"].'" aria-expanded="false" aria-controls="faq_'.$faqs_e["id"].'">'.$faqs_e["qst"].'</button>
+                                  </h5>
+                                  <div class="accordion-collapse collapse" id="faq_'.$faqs_e["id"].'" aria-labelledby="headingOne" data-bs-parent="#accordionFAQ">
+                                    <div class="accordion-body font-lg color-600">'.$faqs_e["answ"].'</div>
+                                  </div>
+                                </div>';
+                        }
+                      }
+                    ?>
                   </div>
                 </div>
                 <div class="col-lg-3">
