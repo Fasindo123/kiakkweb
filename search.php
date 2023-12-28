2b1c5d26f2f0b14293aec3c4be2db3907c592975
PitorParker <huberpetergyorgy@gmail.com>
2023-12-28 13:14:16 +0100

    egy picit javított search

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
-function searchNews($search_query) {
+function searchContent($search_query, $tables_to_search) {
     require_once("dashboard/config.php");
 
     $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -8,14 +8,16 @@ function searchNews($search_query) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT * FROM news WHERE title LIKE '%$search_query%'";
-    $result = $conn->query($sql);
-
     $search_results = [];
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $search_results[] = $row;
+    foreach ($tables_to_search as $table) {
+        $sql = "SELECT * FROM $table WHERE title LIKE '%$search_query%'";
+        $result = $conn->query($sql);
+
+        if ($result !== false && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $search_results[] = $row;
+            }
         }
     }
 
@@ -25,15 +27,18 @@ function searchNews($search_query) {
 }
 
 $search_query = isset($_GET['s']) ? htmlspecialchars($_GET['s']) : '';
-$search_results = searchNews($search_query);
+$tables_to_search = array("news", "courses", "oktatok", "infrastructure", "faqs", "dokumentumok", "tamogatok");
+$search_results = searchContent($search_query, $tables_to_search);
 ?>
 
 <!DOCTYPE html>
 <html lang="hu">
-    <?php require_once('components/head.php'); ?>
+<?php require_once('components/head.php'); ?>
 <body>
+    <?php require_once('components/loader.php'); ?>
+    <?php require_once('components/mobile-menu.php'); ?>
     <?php require_once('components/header.php'); ?>
-<br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <div class="search-txt">
         <p>Keresett szó: <?php echo $search_query; ?></p>
         <p>Találatok: <?php echo count($search_results); ?></p><br><br><br>
@@ -43,19 +48,23 @@ $search_results = searchNews($search_query);
         <div class="row">
             <?php
             foreach ($search_results as $result) {
+                $cover_img = $result['cover_img'] ?? '';
+                $date = $result['date'] ?? '';
+            
                 echo '<div class="col-lg-6">
                         <div class="cardBlog">
-                            <div class="cardImage"><a href="#"><img class="parallax-image" src="' . $result['cover_img'] . '" alt="' . $result['title'] . ' cikk borítóképe"></a></div>
-                            <div class="cardInfo scroll-move-up-2"><a class="tag-link" href="#">' . $result['date'] . '</a><a class="link-blog" href="#">
+                            <div class="cardImage"><a href="#"><img class="parallax-image" src="' . $cover_img . '" alt="' . $result['title'] . ' cikk borítóképe"></a></div>
+                            <div class="cardInfo scroll-move-up-2"><a class="tag-link" href="#">' . $date . '</a><a class="link-blog" href="#">
                             <h4 class="color-light-900 text-opacity">' . $result['title'] . '</h4></a></div>
                         </div>
                     </div>';
             }
+            
             ?>
         </div>
     </div>
 
     <?php require_once("components/footer.php"); ?>
-
+    <?php require_once("components/scripts.php"); ?>
 </body>
 </html>
