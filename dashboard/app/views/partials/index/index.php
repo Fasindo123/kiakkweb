a2639a86a9cfda9ff2387f09f25d97c83dbdbb89
Máté <mtszita2006@gmail.com>
2024-01-02 16:34:03 +0100

    dashboard magyarosítás

--- dashboard/app/views/partials/index/index.php
+++ dashboard/app/views/partials/index/index.php
@@ -9,7 +9,7 @@
                         <div class="">
                             <div class="fadeIn animated mb-4">
                                 <div class="text-capitalize">
-                                    <h2 class="text-capitalize">Welcome To <?php echo SITE_NAME ?></h2>
+                                    <h2 class="text-capitalize"><?php echo SITE_NAME ?> - Kezelőfelület</h2>
                                 </div>
                             </div>
                         </div>
@@ -19,20 +19,20 @@
                         
                         <div  class="bg-light p-3 animated fadeIn page-content">
                             <div>
-                                <h4><i class="fa fa-key"></i> User Login</h4>
+                                <h4><i class="fa fa-key"></i> Bejelentkezés</h4>
                                 <hr />
                                 <?php 
                                 $this :: display_page_errors(); 
                                 ?>
                                 <form name="loginForm" action="<?php print_link('index/login/?csrf_token=' . Csrf::$token); ?>" class="needs-validation form page-form" method="post">
                                     <div class="input-group form-group">
-                                        <input placeholder="Username Or Email" name="username"  required="required" class="form-control" type="text"  />
+                                        <input placeholder="Felhasználónév vagy email" name="username"  required="required" class="form-control" type="text"  />
                                         <div class="input-group-append">
                                             <span class="input-group-text"><i class="form-control-feedback fa fa-user"></i></span>
                                         </div>
                                     </div>
                                     <div class="input-group form-group">
-                                        <input  placeholder="Password" required="required" v-model="user.password" name="password" class="form-control " type="password" />
+                                        <input  placeholder="Jelszó" required="required" v-model="user.password" name="password" class="form-control " type="password" />
                                         <div class="input-group-append">
                                             <span class="input-group-text"><i class="form-control-feedback fa fa-key"></i></span>
                                         </div>
@@ -41,7 +41,7 @@
                                         <div class="col-6">
                                             <label class="">
                                                 <input value="true" type="checkbox" name="rememberme" />
-                                                Remember Me
+                                                Jegyezz meg!
                                             </label>
                                         </div>
                                     </div>
@@ -50,7 +50,7 @@
                                             <i class="load-indicator">
                                                 <clip-loader :loading="loading" color="#fff" size="20px"></clip-loader> 
                                             </i>
-                                            Login <i class="fa fa-key"></i>
+                                            Bejelentkezés <i class="fa fa-key"></i>
                                         </button>
                                     </div>
                                     <hr />
